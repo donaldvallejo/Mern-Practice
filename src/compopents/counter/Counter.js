08c5b51fc1fd78e803d3602fd6d4121ff138846e
donald vallejo <donny.vallejo@gmail.com>
2021-07-08 13:32:47 -0700

    button

--- src/compopents/counter/Counter.js
+++ src/compopents/counter/Counter.js
@@ -3,23 +3,29 @@ import React from 'react'
 const Counter = () => {
     const [count, setcount] = React.useState(0)
     
-    const increment = () => {
-        setcount(count => count + 1)
-    }
-    const decrement = () => {
-        setcount(count => count - 1)
-    }
+    // const increment = () => {
+    //     setcount(count => count + 1)
+    // }
+    // const decrement = () => {
+    //     setcount(count => count - 1)
+    // }
 
+    const changeVal = (e) => {
+        console.log(e.target.className)
+        if(e.target.className === "plus") {
+            setcount(count + 1)
+        }
+        if(e.target.className === "minus") {
+            setcount(count - 1)
+        }
+    }
     return (
         <div>
-            <button onClick={increment}>+</button>
+            <button className="plus" onClick={changeVal}>+</button>
             <br/>
-            <p className="plus">{count}</p>
+            <button className="minus" onClick={changeVal}>-</button>
             <br/>
-
-            <button onClick={decrement}>-</button>
-            <br/>
-            <p className="minus">{count}</p>
+            <p>{count}</p>
         </div>
     )
 }
