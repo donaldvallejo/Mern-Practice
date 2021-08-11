05991ffa50cdf65fa3178c5c3adb11303413dcc2
donald vallejo <donny.vallejo@gmail.com>
2021-08-11 15:05:23 -0700

    basic search queries without api connection

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
 
 const Display = () => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState({city: "", country: ""});
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(data)
+        // deconstruct the data object
+        const {city, country} = data
+        // check if city country are null or empty and retrun from function
+        if (!city || !country || city.trim() === "" || country.trim() === "") return
+        console.log("will call axios with city:", city, "country:", country)
+
     }
     const handleOnChange = (e) => {
+        // const userInput = e.target.value
+        // console.log(userInput)
+        const name = e.target.name
         const userInput = e.target.value
-        setData(userInput)
-        console.log(userInput)
+        console.log(name, userInput)
+        const copiedData = {...data, [name]: userInput}
+        setData(copiedData)
     }
+
 
     return (
         <>
             <div>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" onChange={handleOnChange} value={data}></input>
-
+                    <input name="city" type="text" onChange={handleOnChange} value={data.city}></input>
+                    <input name="country" type="text" onChange={handleOnChange} value={data.country}></input>
+                    <input type="submit" value="submit"></input>
                 </form>
             </div>
         </>
